ae2ef666764d6fe17238c7888f98cf76a14badac
Bubzone <132301194+Bubzone@users.noreply.github.com>
2024-01-31 18:35:20 +0100

    Add files via upload

---
+++ ciagiArytmetczne.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+	float a, r, wynik;
+	int n;
+	string czyWszystkie;
+	cout << "Program wyznacza n-ty wyraz ciągu arytmetycznego - uzytkownik wybiera, czy ma wyswietlac każdy kolejny az do n-tego\n Podaj pierwszy wyraz ciagu:\n";
+	cin >> a;
+	cout << "Podaj roznice: \n";
+	cin >> r;
+	cout << "Podaj ktory wyraz ciagu chcesz wyswietlic:\n";
+	cin >> n;
+	cout << "Czy chcesz aby program wyswietlil wszystkie wyrazy po kolei?(t/n)\n";
+	cin >> czyWszystkie;
+	if (czyWszystkie == "t")
+	{
+		for (int i = 0; i <= n-1; i++)
+		{
+			wynik = a - (i * r);
+			cout << i+1 << ". wyraz ciagu: " << wynik << endl;
+		}
+	}
+	else if (czyWszystkie == "n")
+	{
+		wynik = a - ((n - 1) * r);
+		cout << n << ". wyraz ciagu: " << wynik << endl;
+	}
+}
---
+++ liczbyParzyste.cpp
@@ -0,0 +1,39 @@
+﻿// test.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+void wypiszParzyste(int a, int b)
+{
+    if (a % 2 == 1) {
+        a++;
+    }
+
+    cout << "liczby parzyste: \n";
+
+    for (int i = a; i <= b; i += 2) {
+        cout << i << " ";
+    }
+}
+int main()
+{
+    int a, b;
+    cout << "Program wypisuje liczby parzyste podane przez uzytkownika!\n Podaj liczbe rozpoczynajaca przedzial: \n";
+    cin >> a;
+    cout << "Podaj liczbe konczaca przedzial\n";
+    cin >> b;
+    wypiszParzyste(a, b);
+
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
