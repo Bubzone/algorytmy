bb45668a919d802e9f761e0b518ef076daa69328
Bubzone <132301194+Bubzone@users.noreply.github.com>
2024-02-02 11:40:22 +0100

    Add files via upload

---
+++ LiczbyPierwsze.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+bool czyPierwsza(int a)
+{
+	for (int j = 2; j < a; j++) {
+		if (a % j == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+
+int main() {
+	int ile;
+	cout << "Program wypisuje n liczb pierwszych\n Podaj ile liczb pierwszych chesz: ";
+	cin >> ile;
+	cout << ile << " liczb(-y) pierwszych(-e): \n 2 ";
+
+	int i = 0;
+	if (ile == 1) {
+		return 0;
+	}
+	else {
+		i=1;
+	}	
+	int j = 2;
+	for (i; i < ile; i++) {
+		bool czyJestPierwsza = false;
+		while (czyJestPierwsza != true) {
+			j++;
+			czyJestPierwsza = czyPierwsza(j);
+		}
+		cout << " " << j << " ";
+	}
+}
