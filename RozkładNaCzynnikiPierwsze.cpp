be9fc21feacae1f8214b4010513f21f0b33c776f
Bubzone <132301194+Bubzone@users.noreply.github.com>
2024-02-17 12:55:53 +0100

    Add files via upload

---
+++ RozkładNaCzynnikiPierwsze.cpp
@@ -0,0 +1,27 @@
+﻿#include <math.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int liczba, i = 2;
+
+    cout << "Program rozbija liczbe na czynniki pierwsze!\n Podaj liczbe: \n";
+    cin >> liczba;
+    int a = liczba;
+    cout << "liczba " << liczba << " rozbija sie nas takie czynniki: \n";
+    while (a != 1 && i <= sqrt(liczba)) {
+        if (a % i == 0) {
+            cout << i << " ";
+            a = a / i;
+            i = 2;
+        }
+        else {
+            i++;
+        }
+    }
+    if (a != 1) {
+        cout << a;
+    }
+}
+
+
